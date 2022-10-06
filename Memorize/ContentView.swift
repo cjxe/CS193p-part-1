43d64dca7a0743c0fcc17b3a8d79cc8d865c0046
mbaranturkmen <kilexey@gmail.com>
2022-10-06 13:32:05 +0100

    import multiple emojis from an array into cards

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,19 +8,88 @@
 import SwiftUI
 
 struct ContentView: View {
+    // Array<String>
+    // [String]
+    // but swift can infer it
+    var emojis = ["🚂", "🚀", "🚁", "🚜"]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        HStack {
+            CardView(content: emojis[0])
+            CardView(content: emojis[1])
+            CardView(content: emojis[2])
+            CardView(content: emojis[3])
+            
         }
-        .padding()
+        .padding(.horizontal)
+        .foregroundColor(.red)
+
     }
 }
+
+struct CardView: View {
+    // Variables must have value in swift
+    // @State makes it a pointer  xrx
+    @State var isFaceUp: Bool = true
+    // To read the docs, hold down the `option` key and select `String`
+    var content: String
+    
+    var body: some View {
+        // ZStack: A view that overlays its subviews, aligning them in both axes
+        ZStack {
+            // Swift "inferrs" the type meaning that Swift looks at the content
+            // and puts the type
+            // let shape: RoundedRectangle = RoundedRectangle(...)
+            let shape = RoundedRectangle(cornerRadius: 25.0)
+            if isFaceUp {
+                shape
+                    .fill()
+                    .foregroundColor(.white)
+                shape
+                    .stroke(lineWidth: 3)
+                Text(content)
+                    .font(.largeTitle)
+            } else {
+                shape
+                    .fill()
+            }
+        }
+        .onTapGesture {
+            // all `View`s in SwiftUI are immutable (cannot be modified)
+            // -> new `View`s are constantly beng rebuilt when a `View` is changed
+            isFaceUp = !isFaceUp
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.dark)
+        ContentView()
+            .preferredColorScheme(.light)
+            
     }
 }
